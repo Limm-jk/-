d4959b63b94bd3bd4e33031ffad324ed6c37a6ac
Limm-jk <201602057@cs-cnu.org>
2020-04-08 17:11:47 +0900

    success Crawling

--- CNUCSECrawling.py
+++ CNUCSECrawling.py
@@ -10,9 +10,15 @@ response = requests.get(url)
 #웹 소스코드 추출
 html = response.text
 
-print(html)
+# print(html) #소스코드 확인
 
-# #소스코드를 파이썬 객체로 변환
-# soup = BeautifulSoup(html, 'html.parser')
+#소스코드를 파이썬 객체로 변환
+soup = BeautifulSoup(html, 'html.parser')
 
-# #
+#필요 요소 추출
+links = soup.select('a')
+
+for link in links:
+    if link.has_attr('href'):
+        if link.get('href').find('article.offset') != -1:
+            print((link.text).strip())
